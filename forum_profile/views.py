5a3b578b588b89c1385cd94bc6754427979424e3
Student <PlagueEvgeny@yandex.ru>
2023-12-16 13:41:53 +0500

    minor profile

--- forum_profile/views.py
+++ forum_profile/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse_lazy
@@ -8,8 +9,15 @@ from forum_profile.forms import ProfileCreationForm
 from forum_profile.models import Profile
 
 
-class ProfileCreateView(CreateView):
+class ProfileCreateView(LoginRequiredMixin, CreateView):
     model = Profile
     form_class = ProfileCreationForm
     template_name = 'forum_profile/create_profile.html'
     success_url = reverse_lazy('profile:create')
+
+    def form_valid(self, form):
+        profile = form.save(commit=False)
+        profile.user = self.request.user
+        profile.save()
+
+        return redirect(self.success_url)
